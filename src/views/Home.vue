9a6c2ff6e3e8bf59c7647400545414b239cec086
Pupan Phonkaew <43066870+Pupan-cpe@users.noreply.github.com>
2021-02-07 00:19:09 +0700

    go

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,78 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div id="watch-example">
+  <p>
+
+  </p>
+  <!-- <p>{{ answer }}</p> -->
+</div>
+    <!-- <div :callback="oneChatCallBackQRScanner(ScanQRcodevalue)" /> -->
+    <div class="col-6 col-s-9">
+      <h1>
+        <a
+          href="https://chat-api.one.th/go_api/api/v1/openScanQRcodeWithValue"
+          target="_blank"
+
+          >nn</a
+        >
+
+        <!-- <button
+@click="getUsers()"
+></button> -->
+        <!-- <a
+          href="https://chat-api.one.th/go_api/api/v1/openScanQRcodeWithValue"
+          target="_blank"
+          class="button"
+          >Scan</a
+
+        > -->
+        <!-- {{ScanQRcodevalue}} -->
+      </h1>
+      {{ this.pupan }}
+      <!-- {{ ScanQRcodevalue }} -->
+      <!-- <p>Chania is the capital of the Chania region on the island of Crete. The city can be divided in two parts, the old town and the modern city.</p> -->
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+// import axios from 'axios'
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      pupan: ''
+    }
+  },
+  // oneChatCallBackQRScanner (ScanQRcodevalue) {
+  //   alert(ScanQRcodevalue)
+  //   console.log(ScanQRcodevalue)
+  // },
+  // created () {
+  //   this.oneChatCallBackQRScanner(ScanQRcodevalue => console.log(ScanQRcodevalue))
+
+  mounted () {
+    // alert(this.pupan)
+    if (this.pupan === undefined) {
+      // alert('hello')
+      console.log(this.pupan)
+    } else {
+
+    }
+  },
+
+  methods: {
+
+    oneChatCallBackQRScanner (ScanQRcodevalue) {
+      this.pupan = ScanQRcodevalue
+      alert('start')
+      if (ScanQRcodevalue === undefined) {
+        console.log('emty')
+      } else {
+        alert(ScanQRcodevalue)
+        console.log(ScanQRcodevalue)
+      }
+    }
+
   }
 }
 </script>
