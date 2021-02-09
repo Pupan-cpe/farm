51c2cd17c0549287f29019fd365268920ae41509
Pupan Phonkaew <43066870+Pupan-cpe@users.noreply.github.com>
2021-02-09 16:44:04 +0700

    finnish [2/9/2021] 16:43 Pupan

---
+++ src/views/my-component.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <div>
+      <a
+        href="https://chat-api.one.th/go_api/api/v1/openScanQRcodeWithValue"
+        target="_blank"
+        >--------Scan QR--------</a
+      >
+    </div>
+
+    <h1>
+
+    </h1>
+         <div id="qrcodecallback">callback:</div>
+  </div>
+
+</template>
+
+<script>
+
+// import callback from './callback.js'
+
+export default {
+  data () {
+    return {
+
+      // callback,
+      // pupan: ''
+
+      created () {
+
+      },
+      mounted () {
+
+        // this.callback = require('./callback.js')
+        // console.log(this.callback)
+      },
+
+      methods: {
+
+        // oneChatCallBackQRScanner (qrcode) {
+        //   // callback = qrcode
+        //   // alert(callback)
+        // }
+
+      }
+
+    }
+  }
+
+}
+
+</script>
+
+<style lang="scss" scoped>
+</style>
